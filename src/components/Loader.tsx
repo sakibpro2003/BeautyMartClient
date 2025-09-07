27d0a8053a405f4ccf755dbfddbdf430a5dc0255
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-09-07 16:15:24 +0600

    refactor: loader animation added

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
-// import loader from "../Loader.gif";
-import logo from "../../public/logo/logo.png"
+import logo from "../../public/logo/logo.png";
 
 const Loader = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <Image unoptimized src={logo} alt="loader" width={100} height={100}></Image>
+      <Image
+        unoptimized
+        src={logo}
+        alt="loader"
+        width={100}
+        height={100}
+        className="animate-pulse"
+      />
     </div>
   );
 };
